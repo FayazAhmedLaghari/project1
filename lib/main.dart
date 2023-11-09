6aa047fad820ac615b7e297b0d15ae60aa780867
FayazAhmedLaghari <fayaz786@gmail.com>
2023-11-09 21:57:45 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+void main(){
+  runApp(MyProject());
+}
+class MyProject extends StatelessWidget{
+  @override
+  Widget build(BuildContext context)
+  {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyHomePage(),
+
+    );
+  }
+}
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        actions: [
+          Icon(Icons.more_vert_outlined),
+          Icon(Icons.notifications),
+          Icon(Icons.search),
+
+
+        ],
+        backgroundColor: Colors.pink,
+        title:Text('Welome to Mobile App Developer Flutter',style: TextStyle(fontSize: 20,color: Colors.white)
+      ),
+
+      ),
+    );
+  }
+}
+
+
